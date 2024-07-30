23d183c3e68e5dc8337530ed36c220ca40042e7b
soham <soham.kolte15@gmail.com>
2024-07-31 00:56:43 +0530

    modified property.html,script.js,styles.css

--- script.js
+++ script.js
@@ -38,3 +38,23 @@ function geocode(query) {
         });
 }
 
+
+function updatePrice() {
+    const priceRange = document.getElementById('priceRange');
+    const minPrice = document.getElementById('minPrice');
+    const maxPrice = document.getElementById('maxPrice');
+
+    minPrice.innerText = '0';
+    maxPrice.innerText = priceRange.value;
+}
+
+function filterBedrooms(bedroomType) {
+    console.log('Filter by bedrooms:', bedroomType);
+    // Implement the logic to filter listings based on the number of bedrooms
+}
+
+function filterPostedBy(postedBy) {
+    console.log('Filter by posted by:', postedBy);
+    // Implement the logic to filter listings based on who posted
+}
+
