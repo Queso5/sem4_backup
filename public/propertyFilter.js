8f8e3d0af7c277fb72ff59fc373026a4363e0b62
SumitMishra <mishrasumeet0707@gmail.com>
2024-08-02 23:20:46 +0530

    added propertyFilter.js

---
+++ public/propertyFilter.js
@@ -0,0 +1,18 @@
+function updatePrice() {
+    const priceRange = document.getElementById('priceRange');
+    const minPrice = document.getElementById('minPrice');
+    const maxPrice = document.getElementById('maxPrice');
+
+    minPrice.innerText = '0';
+    maxPrice.innerText = priceRange.value;
+}
+
+function filterBedrooms(bedroomType) {
+    console.log('Filter by bedrooms:', bedroomType);
+    // Implement the logic to filter listings based on the number of bedrooms
+}
+
+function filteramenities(amenities) {
+    console.log('Filter amenities:', amenities);
+    // Implement the logic to filter listings based on amenities
+}
